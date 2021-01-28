d0c54c6ce2c73dc4ad617d6742503c1ba0410649
Maneesh <maneesh@uninity.com>
2021-01-28 19:27:37 +0530

    DB connectin string added

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const profile = require('./controllers/profile');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'maneesh',
-      database : 'face-recognition'
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
     }
 });
 
